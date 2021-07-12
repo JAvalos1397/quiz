fd77a9b4c8ede93f98b3b4da0f19f00d7645fa91
Jose Avalos <avaljose@hotmai.com>
2021-07-12 16:53:58 -0500

    adding timer

--- Assets/script.js
+++ Assets/script.js
@@ -2,13 +2,22 @@ var startButton = document.getElementById('start-btn')
 var questionContainerEl = document.getElementById('questionContainer')
 var shuffledQuestion, currentQuestionIndex
 var answerBtnEl = document.getElementById('answer-btn')
-
+var timerEl = document.getElementById('timerContainer')
 let questionEl = document.getElementById('question')
 
 
 startButton.addEventListener('click', function() {
     startGame()
+    countdown()
 })
+
+function countdown() {
+
+var sec = 100;
+var timer= setInterval(function(){
+    document.getElementById(timer).innerHTML="0"
+})
+}
 
 function startGame() {
     console.log('Started');
@@ -29,14 +38,17 @@ function showQuestion(question) {
 
 function selectAnswer() {
 
-}
 
+}
+//https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified
 var question = [
     {
         question: "What is 5+5?",
         Answer: [
             {text:"10", correct: true},
-            {text:"22", correct: false }
+            {text:"25", correct: false },
+            {text:"30", correct: false },
+            {text:"40", correct: false }
         ]
     }
 ]
