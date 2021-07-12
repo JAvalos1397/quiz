b3873134fa43007195d2e3cf2cf86b28ca0361c7
Jose Avalos <avaljose@hotmai.com>
2021-07-12 17:57:42 -0500

    randomizer

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,10 @@
 var startButton = document.getElementById('start-btn')
 var questionContainerEl = document.getElementById('questionContainer')
-var shuffledQuestion, currentQuestionIndex
 var answerBtnEl = document.getElementById('answer-btn')
+var questionEl = document.getElementById('question')
 var timerEl = document.getElementById('timerContainer')
-let questionEl = document.getElementById('question')
+
+let shuffledQuestion, currentQuestionIndex
 
 
 startButton.addEventListener('click', function() {
@@ -12,18 +13,27 @@ startButton.addEventListener('click', function() {
 })
 
 function countdown() {
-
-var sec = 100;
+timerEl.classList.remove("hide")
+var secondsLeft = 100;
 var timer= setInterval(function(){
-    document.getElementById(timer).innerHTML="0"
-})
+        secondsLeft--;
+        document.getElementById("timer").textContent= secondsLeft;
+
+        if(secondsLeft === 0){
+        clearInterval(timer);
+        
+        //finish game
+        }
+  }, 1000);
 }
+
+
 
 function startGame() {
     console.log('Started');
     startButton.classList.add("hide");
     shuffledQuestion = question.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
+    currentQuestionIndex= 0
     questionContainerEl.classList.remove("hide")
     setNextQuestion()
 }
@@ -34,17 +44,27 @@ function setNextQuestion() {
 
 function showQuestion(question) {
     questionEl.innerText = question.question
+    question.answer.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer);
+        answerBtnEl.appendChild(button);
+        
+    })
 }
 
 function selectAnswer() {
 
-
 }
-//https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified
+
 var question = [
     {
-        question: "What is 5+5?",
-        Answer: [
+        question: "What is 5+5?!",
+        answer: [
             {text:"10", correct: true},
             {text:"25", correct: false },
             {text:"30", correct: false },
